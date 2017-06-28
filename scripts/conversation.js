11edac0cb92009f37cf819f6ccd3dc8cbdb69979
Devendra Nalawade <devendra@Devendras-MacBook-Pro.local>
2017-06-28 23:23:23 +0900

    additional conversation added

--- scripts/conversation.js
+++ scripts/conversation.js
@@ -1,27 +1,41 @@
 var request = require('request');
 
+const questionData = [
+    {
+        'question': 'hello|hey there|こにちわ|wassup$/i',
+        'answer': 'Hey there...'
+    },
+    {
+        'question': /what is your name|おなまえわなんですか|name/,
+        'answer': ['Hey, I am Devendra']
+    },
+    {
+        'question': /how are you|お元気ですか/,
+        'answer': ['Yeah, I am doing pretty good']
+    },
+    {
+        'question': /age|birthdate/,
+        'answer': [
+            "My birthdate is 13th February 1986.",
+            "I am 31 years old!"
+        ]
+    }
+];
+
 var conversation = function(robot) {
-
-    robot.hear(/I like pie/, function(res) {
-        res.emote('okay, I will bring some next time we meet...');
-    });
-
-    robot.hear(/How are you buddy/, function(res) {
-        res.send('I am pretty okay sir, how about you...');
-    });
-
-    robot.hear(/find results for (.*)/i, function(res) {
-        const query = res.match[1];
-        res.reply('searching result for ' + query);
-
-        request('https://google.co.jp/webhp?hl=en&q=query', function(error, response, body) {
-            console.log('error:', error);
-            console.log('status:', response && response.statusCode);
-            console.log('body', body);
-            res.send(body);
+    questionData.forEach(function(data) {
+        robot.hear(data.question, function(res) {
+            if (Array.isArray(data.answer)) {
+                data.answer.forEach(function(ans, index) {
+                    setTimeout(function() {
+                        res.emote(ans);
+                    }, 3000 * index);
+                });
+            } else {
+                res.emote(data.answer);
+            }
         });
     });
-
 }
 
 module.exports = conversation;
